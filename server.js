ba603b50f01fa485abc85483ad58a7a68a46c4d3
astrodemo885 <sahti123@proton.me>
2026-02-25 09:22:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -61,9 +61,11 @@ app.post("/pay/:id", (req, res) => {
     reservation
   });
 });
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
+});
+app.get("/", (req, res) => {
+  res.send("OK");
 });
