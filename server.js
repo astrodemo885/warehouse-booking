863a566a7cc50a1934a8d2a7cfe7cc08d69a5fb4
astrodemo885 <sahti123@proton.me>
2026-02-25 11:51:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("OK"));
+app.get("/", (req, res) => {
+  res.send("OK");
+});
 
 app.get("/items", (req, res) => {
   res.json([
@@ -15,6 +17,7 @@ app.get("/items", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Listening on " + PORT);
+  console.log("Server running on port " + PORT);
 });
